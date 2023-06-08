d4c3a4629e40afca5b9f3998f577d6d41f698079
shinckel <sofiahdias@gmail.com>
2023-06-08 14:53:06 +0100

    feat: first commit

---
+++ header/ft_fractol.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fractol.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shinckel <shinckel@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/08 14:18:55 by shinckel          #+#    #+#             */
+/*   Updated: 2023/06/08 14:25:56 by shinckel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_FRACTOL_H
+# define FT_FRACTOL_H
+
+# include "minilibx_mms_20191025_beta/mlx.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <X11/keysym.h>
+# include <X11/X.h>
+# include <math.h>
+# include <string.h>
+
+#endif
